3471da64025feb59398447db020fb408a367c3d9
Julia Peter <juliagpeter0@gmail.com>
2024-11-23 03:29:47 -0300

    feat: melhorias no cadastro

---
+++ cypress/e2e/cadastro-ok.cy.js
@@ -0,0 +1,13 @@
+describe("Cadastro Adopet", () => {
+  beforeEach(() => {
+    cy.visit("https://adopet-frontend-cypress.vercel.app/");
+    cy.get('[data-test="register-button"]').click();
+  });
+  it("Deve preencher os campos e concluir o cadastro", () => {
+    cy.get('[data-test="input-name"]').type("Júlia");
+    cy.get('[data-test="input-email"]').type("jlgirlyt@gmail.com");
+    cy.get('[data-test="input-password"]').type("Senha123");
+    cy.get('[data-test="input-confirm-password"]').type("Senha123");
+    cy.get('[data-test="submit-button"]').click();
+  });
+});
