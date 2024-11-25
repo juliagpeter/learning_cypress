008edc93c120b433524f6d7f1f641e86990c76f0
Julia Peter <juliagpeter0@gmail.com>
2024-11-25 16:37:58 -0300

    feat: command signup

--- cypress/e2e/cadastro-ok.cy.js
+++ cypress/e2e/cadastro-ok.cy.js
@@ -4,10 +4,6 @@ describe("Cadastro Adopet", () => {
     cy.get('[data-test="register-button"]').click();
   });
   it("Deve preencher os campos e concluir o cadastro", () => {
-    cy.get('[data-test="input-name"]').type("Júlia");
-    cy.get('[data-test="input-email"]').type("jlgirlyt@gmail.com");
-    cy.get('[data-test="input-password"]').type("Senha123");
-    cy.get('[data-test="input-confirm-password"]').type("Senha123");
-    cy.get('[data-test="submit-button"]').click();
+    cy.signUp("Júlia", "jlgirlyt@gmail.com", "Senha123");
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,6 +4,13 @@ Cypress.Commands.add('login', (email, password) => {
     cy.get('[data-test="submit-button"]').click(); 
 })
 
+Cypress.Commands.add('signUp', (nome, email, password) => {
+    cy.get('[data-test="input-name"]').type(nome);
+    cy.get('[data-test="input-email"]').type(email);
+    cy.get('[data-test="input-password"]').type(password);
+    cy.get('[data-test="input-confirm-password"]').type(password);
+    cy.get('[data-test="submit-button"]').click();
+})
 
 
 // ***********************************************
